c05b3d22e620ac94d258bfae7781b5c235ecf720
rfzanetti <rfzanetti@gmail.com>
2017-06-01 18:23:14 -0300

    small k-fold-CV template changes

--- zane_changes/k-fold-CV-template.m.m
+++ zane_changes/k-fold-CV-template.m.m
@@ -1,8 +1,8 @@
 clc; clear; close all
 
 dados = csvread('basefinal.csv');
-
 k = 10;
+acc = 0
 
 num_amostras = size(dados, 1);
 tam_particao = ceil(num_amostras / k);
@@ -13,6 +13,10 @@ for (i = 0 : k-1)
 	inicio = (i * tam_particao) + 1;
 	fim = min(inicio + tam_particao - 1, num_amostras);
 
-	test_data = dados(inicio:fim, :);
 	train_data = [   (dados((1 : (inicio - 1)), :))   ;   (dados((fim + 1) : num_amostras, :))   ]; 
+	test_data = dados(inicio:fim, :);
+
+	acc = acc + some_algorithm(train_data, test_data)
 end
+
+acc = acc / 10
